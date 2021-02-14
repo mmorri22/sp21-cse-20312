4f1d80d589c3a8f1a65da8ce6b3d58b89d204a3f
Matthew Morrison <mmorri22@nd.edu>
2021-02-13 22:16:33 -0500

    setup update

---
+++ CC02/setup.sh
@@ -0,0 +1,9 @@
+mkdir exe inc obj src
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/CC02/JK_test_1.txt
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/CC02/JK_test_2.txt
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/CC02/JK_test_3.txt
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/CC02/JK_test_4.txt
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/CC02/BCD_test_1.txt
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/CC02/BCD_test_2.txt
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/CC02/BCD_test_3.txt
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/CC02/BCD_test_4.txt
