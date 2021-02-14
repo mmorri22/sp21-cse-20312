3ca4fbe33341d51974c22546aeb925f3abfeb22a
Matthew Morrison <mmorri22@nd.edu>
2021-02-14 10:12:20 -0500

    CC02

--- CC02/setup.sh
+++ CC02/setup.sh
@@ -3,9 +3,9 @@ wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/CC02/JK_test
 wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/CC02/JK_test_2.txt
 wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/CC02/JK_test_3.txt
 wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/CC02/JK_test_4.txt
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/CC02/BCD_test_1.txt
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/CC02/BCD_test_2.txt
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/CC02/BCD_test_3.txt
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/CC02/BCD_test_4.txt
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/CC02/BDC_test_1.txt
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/CC02/BDC_test_2.txt
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/CC02/BDC_test_3.txt
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/CC02/BDC_test_4.txt
 wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/CC02/test.sh
 chmod a+rx test.sh 
