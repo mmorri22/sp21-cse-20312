b740924c6eac5b8edc699c9a4802a8f5ff01b0d9
Matthew Morrison <mmorri22@nd.edu>
2021-02-24 18:56:53 -0500

    Lec 07 Update

---
+++ Lec07/inc/student.h
@@ -0,0 +1,29 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include "addr.h"
+
+class Student{
+	
+	private:
+	
+		STRING stu_first;
+		STRING stu_second;
+		addr stu_addr;
+	
+	public:
+	
+		/* Default constructor */
+		Student() : stu_first(), stu_second(), stu_addr() {}
+		
+		/* Overloaded constructor */
+		Student( const STRING& first_in, const STRING& second_in, const addr& addr_in )
+			: stu_first( first_in ),
+			stu_second( second_in ),
+			stu_addr( addr_in ) {}
+			
+		friend std::ostream& operator<<( std::ostream& out, const Student& print_stu );
+	
+};
+
+#endif
