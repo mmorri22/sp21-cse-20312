f56c38b0fc0b3d36d1d350315492abea1f4f9c11
Matthew Morrison <mmorri22@nd.edu>
2021-02-24 18:44:19 -0500

    Lec 07 Update

--- Lec07/src/addr_test.cpp
+++ Lec07/src/addr_test.cpp
@@ -5,7 +5,7 @@ int main(){
 	
 	addr addr_0( 123, "Main St.", "South Bend", "IN", 46530 ); 
 	
-	addr addr_1( 404, "Street Not Found", "Nowhere", "IN", 46556 ); 
+	const addr addr_1( 404, "Street Not Found", "Nowhere", "IN", 46556 ); 
 	
 	COUT << addr_0 << ENDL;
 	
