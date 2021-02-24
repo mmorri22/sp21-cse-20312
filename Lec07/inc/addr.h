8315d670d67d24f9bf22d6a84ccb5c42758745ee
Matthew Morrison <mmorri22@nd.edu>
2021-02-24 17:37:52 -0500

    Lec 07 Update

---
+++ Lec07/inc/addr.h
@@ -0,0 +1,36 @@
+/* Base class */
+
+/* Step 1 - Guard Header Files */
+
+
+/* Step 2 - Must include iostream and string */
+
+
+/* Step 3 - Do define for COUT, ENDL, and STRING */ 
+
+
+class addr{
+	
+	private:
+	
+		unsigned int street_num;
+		STRING street_name;
+		STRING street_city;
+		STRING street_state;
+		unsigned int zip_code;
+		
+	public:
+	
+		addr(); : street_num(), street_name(), street_city(), street_state(), zip_code() {}
+		
+		/* Step 4 - Write the overloaded constructor for taking in the address */
+		
+		
+		/* Step 5 - Write a friend operator declaration to print the address to the output */
+	
+	
+	
+};
+
+
+/* Step 1 - Guard Header Files */
---
+++ Lec07/src/addr.cpp
@@ -0,0 +1,9 @@
+#include "../inc/addr.h"
+
+addr::addr() : street_num(), street_name(), street_city(), street_state(), zip_code() {}
+
+
+/* Step 4 - Write the overloaded constructor declaration for taking in the address */
+
+
+/* Step 5 - Write a friend operator definition to print the address to the output */
