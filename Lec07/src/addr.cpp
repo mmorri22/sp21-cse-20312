f673396a7c4490e415a1217eb0f4c41f1b31be1d
Matthew Morrison <mmorri22@nd.edu>
2021-02-24 18:27:16 -0500

    Lec 07 Update

--- Lec07/src/addr.cpp
+++ Lec07/src/addr.cpp
@@ -3,7 +3,7 @@
 addr::addr() : street_num(), street_name(), street_city(), street_state(), zip_code() {}
 
 
-/* Step 4 - Write the overloaded constructor declaration for taking in the address */
-
+/* Step 4 - Write the overloaded constructor definition member initialization list for taking in the address */
+addr::addr( const unsigned int& num_in, const STRING& name_in, const STRING& city_in, const STRING& state_in, const unsigned int& zip_in ) : 
 
 /* Step 5 - Write a friend operator definition to print the address to the output */
