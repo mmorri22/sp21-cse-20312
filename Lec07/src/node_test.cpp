8e5cba2e9260dcf588f9abaa19b3e24f2ddf66ec
Matthew Morrison <mmorri22@nd.edu>
2021-02-24 22:25:47 -0500

    Lec 07 Push

--- Lec07/src/node_test.cpp
+++ Lec07/src/node_test.cpp
@@ -7,11 +7,13 @@ int main(){
 	
 	const addr addr_1( 404, "Street Not Found", "Nowhere", "IN", 46556 ); 
 	
+	addr addr_2( 408, "Street Not Found", "Nowhere", "IN", 46556 ); 
+	
 	Student stu_1( "Brian", "Kelly", addr_0 );
 	
 	const Student stu_2( "Matthew", "Morrison", addr_1 ); 
 	
-	Student stu_3( "Matthew", "Morrison", addr_1 ); 
+	Student stu_3( "Peter", "Bui", addr_2 ); 
 	
 	/* Create the nodes */
 	node< Student > node1( stu_1 );
@@ -21,7 +23,6 @@ int main(){
 	node< Student > node3( stu_3 );
 	
 	/* Example showing connecting */
-	/* node1.prev = NULL;
 	
 	node1.next = &node2;
 	
@@ -29,7 +30,10 @@ int main(){
 	
 	node2.next = &node3;
 	
-	node3.prev = &node2; */
+	node3.prev = &node2;
+	
+	/* Step 9 - Iterate and print */
+
 	
 	return 0;
 	
