84d604634d2a58f82c7afd7a4b7e3436596be8e3
Matthew Morrison <mmorri22@nd.edu>
2021-02-24 19:47:23 -0500

    Lec 07 Update

--- Lec07/src/student.cpp
+++ Lec07/src/student.cpp
@@ -8,12 +8,72 @@ Student::Student( const STRING& first_in, const STRING& second_in, const addr& a
 	: stu_first( first_in ),
 	stu_second( second_in ),
 	stu_addr( addr_in ) {}
+
+/* Step 6 - Example of Overloaded Operators for Student */
+
+
+
+/* Other operators */
+bool Student::operator>=( const Student& rhs ){
 	
+	if( stu_last < rhs.stu_last )
+		return false;
+	
+	else{
+		
+		if( stu_first < rhs.stu_first )
+			return false;
+		
+		return true;
+	}
+}
+
+bool Student::operator<( const Student& rhs ){
+	
+	if( stu_last < rhs.stu_last )
+		return true;
+	
+	else{
+		
+		if( stu_first < rhs.stu_first ){
+			
+			return true;
+		}
+		
+		return false;
+	}
+}
+
+bool Student::operator<=( const Student& rhs ){
+	
+	if( stu_last > rhs.stu_last )
+		return false;
+	
+	else if( stu_last < rhs.stu_last )
+		return true;
+	
+	/* Last Names are equal */
+	else{
+		
+		if( stu_first <= rhs.stu_first )
+			return true;
+			
+		return false;
+	}
+	
+}
+bool Student::operator==( const Student& rhs ){
+	
+	return ( stu_last == rhs.stu_last ) && ( stu_first == rhs.stu_first );
+	
+}
+
+
 	
 /* Friend Operator */
 std::ostream& operator<<( std::ostream& out, const Student& print_stu ){
 	
-	/* Step 6 - Calling friend operator with private aggregated member address */
+	/* Step 7 - Calling friend operator with private aggregated member address */
 	
 	return out;
 }
