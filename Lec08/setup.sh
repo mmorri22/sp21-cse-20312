d7bcf010615f71e4cf1da406f8232f5f1ed131db
Matthew Morrison <mmorri22@nd.edu>
2021-03-04 09:05:41 -0500

    Script Upload

--- Lec08/setup.sh
+++ Lec08/setup.sh
@@ -1,29 +1,76 @@
-mkdir src inc exe obj
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec07/Makefile
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec07/inc/addr.h
+mkdir src inc
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec08/Makefile
+make initialize
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec08/DLList_Test.txt
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec08/inc/addr.h
 cp addr.h inc/addr.h 
 rm addr.h
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec07/inc/DLList.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec08/inc/DLList.h
 cp DLList.h inc/DLList.h 
 rm DLList.h
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec07/inc/Node.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec08/inc/Node.h
 cp Node.h inc/Node.h 
 rm Node.h
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec07/inc/student.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec08/inc/student.h
 cp student.h inc/student.h 
 rm student.h
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec07/src/addr.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec08/inc/point.h
+cp point.h inc/point.h 
+rm point.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec08/inc/line.h
+cp line.h inc/line.h 
+rm line.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec08/inc/stack.h
+cp stack.h inc/stack.h 
+rm stack.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec08/inc/queue.h
+cp queue.h inc/queue.h 
+rm queue.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec08/inc/max_stack.h
+cp max_stack.h inc/max_stack.h 
+rm max_stack.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec08/inc/SLList.h
+cp SLList.h inc/SLList.h 
+rm SLList.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec08/inc/Tower.h
+cp Tower.h inc/Tower.h 
+rm Tower.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec08/src/addr.cpp
 cp addr.cpp src/addr.cpp 
 rm addr.cpp
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec07/src/addr_test.cpp
-cp addr_test.cpp src/addr_test.cpp 
-rm addr_test.cpp
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec07/inc/DLList_Test.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec08/inc/DLList_Test.cpp
 cp DLList_Test.cpp inc/DLList_Test.cpp 
 rm DLList_Test.cpp 
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec07/src/student.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec08/inc/DLList_Test2.cpp
+cp DLList_Test2.cpp inc/DLList_Test2.cpp 
+rm DLList_Test2.cpp 
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec08/src/student.cpp
 cp student.cpp src/student.cpp 
 rm student.cpp
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec07/src/student_test.cpp
-cp student_test.cpp src/student_test.cpp 
-rm student_test.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec08/src/point.cpp
+cp point.cpp src/point.cpp 
+rm point.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec08/src/line.cpp
+cp line.cpp src/line.cpp 
+rm line.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec08/src/stackTest.cpp
+cp stackTest.cpp src/stackTest.cpp 
+rm stackTest.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec08/src/wrapperStack.cpp
+cp wrapperStack.cpp src/wrapperStack.cpp 
+rm wrapperStack.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec08/src/wrapperQueue.cpp
+cp wrapperQueue.cpp src/wrapperQueue.cpp 
+rm wrapperQueue.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec08/src/hanoi.cpp
+cp hanoi.cpp src/hanoi.cpp 
+rm hanoi.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec08/src/max_stack_sol.cpp
+cp max_stack_sol.cpp src/max_stack_sol.cpp 
+rm max_stack_sol.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec08/src/shortestPath.cpp
+cp shortestPath.cpp src/shortestPath.cpp 
+rm shortestPath.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec08/src/shortestPath.cpp
+cp shortestPath.cpp src/shortestPath.cpp 
+rm shortestPath.cpp
