95a75e2c77129e83d2a85cf5890e6d13b0f23533
Matthew Morrison <mmorri22@nd.edu>
2021-03-05 14:02:34 -0500

    setup

--- CC04/setup.sh
+++ CC04/setup.sh
@@ -1,3 +1,6 @@
 mkdir src inc
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/CC04/Makefile
+make initialize
 wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/CC04/inc/DLList.h
 cp DLList.h inc/DLList.h
+rm DLList.h
