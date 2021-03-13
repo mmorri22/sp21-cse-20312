4061b3aa0e1b09c68addd32dcdaab07c1865111b
Matthew Morrison <mmorri22@nd.edu>
2021-03-13 11:33:38 -0500

    Lec11 Fix

--- Lec11/setup.sh
+++ Lec11/setup.sh
@@ -1,95 +1,95 @@
 mkdir src inc
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec11/Makefile
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20212/main/Lec11/Makefile
 make initialize
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec11/test.sh
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20212/main/Lec11/test.sh
 chmod a+rx test.sh
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec11/inc/LinearProbe1.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20212/main/Lec11/inc/LinearProbe1.h
 cp LinearProbe1.h inc/LinearProbe1.h
 rm LinearProbe1.h
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec11/inc/LinearProbe2.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20212/main/Lec11/inc/LinearProbe2.h
 cp LinearProbe2.h inc/LinearProbe2.h
 rm LinearProbe2.h
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec11/inc/LinearProbe3.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20212/main/Lec11/inc/LinearProbe3.h
 cp LinearProbe3.h inc/LinearProbe3.h
 rm LinearProbe3.h
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec11/inc/LinearProbe4.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20212/main/Lec11/inc/LinearProbe4.h
 cp LinearProbe4.h inc/LinearProbe4.h
 rm LinearProbe4.h
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec11/inc/LinearProbe5.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20212/main/Lec11/inc/LinearProbe5.h
 cp LinearProbe5.h inc/LinearProbe5.h
 rm LinearProbe5.h
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec11/inc/LinearProbe6.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20212/main/Lec11/inc/LinearProbe6.h
 cp LinearProbe6.h inc/LinearProbe6.h
 rm LinearProbe6.h
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec11/inc/LinearProbe7.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20212/main/Lec11/inc/LinearProbe7.h
 cp LinearProbe7.h inc/LinearProbe7.h
 rm LinearProbe7.h
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec11/inc/LinearProbe.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20212/main/Lec11/inc/LinearProbe.h
 cp LinearProbe.h inc/LinearProbe.h
 rm LinearProbe.h
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec11/src/LPTest1.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20212/main/Lec11/src/LPTest1.cpp
 cp LPTest1.cpp src/LPTest1.cpp
 rm LPTest1.cpp
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec11/src/LPTest2.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20212/main/Lec11/src/LPTest2.cpp
 cp LPTest2.cpp src/LPTest2.cpp
 rm LPTest2.cpp
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec11/src/LPTest3.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20212/main/Lec11/src/LPTest3.cpp
 cp LPTest3.cpp src/LPTest3.cpp
 rm LPTest3.cpp
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec11/src/LPTest4.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20212/main/Lec11/src/LPTest4.cpp
 cp LPTest4.cpp src/LPTest4.cpp
 rm LPTest4.cpp
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec11/src/LPTest5.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20212/main/Lec11/src/LPTest5.cpp
 cp LPTest5.cpp src/LPTest5.cpp
 rm LPTest5.cpp
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec11/src/LPTest6.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20212/main/Lec11/src/LPTest6.cpp
 cp LPTest6.cpp src/LPTest6.cpp
 rm LPTest6.cpp
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec11/src/LPTest7.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20212/main/Lec11/src/LPTest7.cpp
 cp LPTest7.cpp src/LPTest7.cpp
 rm LPTest7.cpp
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec11/src/LPTest8.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20212/main/Lec11/src/LPTest8.cpp
 cp LPTest8.cpp src/LPTest8.cpp
 rm LPTest8.cpp
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec11/src/LPTest.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20212/main/Lec11/src/LPTest.cpp
 cp LPTest.cpp src/LPTest.cpp
 rm LPTest.cpp
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec11/src/Prob1.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20212/main/Lec11/src/Prob1.cpp
 cp Prob1.cpp src/Prob1.cpp
 rm Prob1.cpp
-wget https://raw.githubusercontent.com/mmorri22/sp22-cse-20322/main/Lec22/src/Prob2.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20322/main/Lec22/src/Prob2.cpp
 cp Prob2.cpp src/Prob2.cpp
 rm Prob2.cpp
-wget https://raw.githubusercontent.com/mmorri55/sp55-cse-50355/main/Lec55/src/Prob5.cpp
+wget https://raw.githubusercontent.com/mmorri21/sp21-cse-50321/main/Lec21/src/Prob5.cpp
 cp Prob5.cpp src/Prob5.cpp
 rm Prob5.cpp
-wget https://raw.githubusercontent.com/mmorri66/sp66-cse-60366/main/Lec66/src/Prob6.cpp
+wget https://raw.githubusercontent.com/mmorri21/sp21-cse-60321/main/Lec21/src/Prob6.cpp
 cp Prob6.cpp src/Prob6.cpp
 rm Prob6.cpp
-wget https://raw.githubusercontent.com/mmorri77/sp77-cse-70377/main/Lec77/src/Prob7.cpp
+wget https://raw.githubusercontent.com/mmorri21/sp21-cse-70321/main/Lec21/src/Prob7.cpp
 cp Prob7.cpp src/Prob7.cpp
 rm Prob7.cpp
-wget https://raw.githubusercontent.com/mmorri77/sp77-cse-70377/main/Lec77/src/PQTest.cpp
+wget https://raw.githubusercontent.com/mmorri21/sp21-cse-70321/main/Lec21/src/PQTest.cpp
 cp PQTest.cpp src/PQTest.cpp
 rm PQTest.cpp
-wget https://raw.githubusercontent.com/mmorri77/sp77-cse-70377/main/Lec77/src/testMaxHeap2.cpp
+wget https://raw.githubusercontent.com/mmorri21/sp21-cse-70321/main/Lec21/src/testMaxHeap2.cpp
 cp testMaxHeap2.cpp src/testMaxHeap2.cpp
 rm testMaxHeap2.cpp
-wget https://raw.githubusercontent.com/mmorri77/sp77-cse-70377/main/Lec77/src/testMaxHeap3.cpp
+wget https://raw.githubusercontent.com/mmorri21/sp21-cse-70321/main/Lec21/src/testMaxHeap3.cpp
 cp testMaxHeap3.cpp src/testMaxHeap3.cpp
 rm testMaxHeap3.cpp
-wget https://raw.githubusercontent.com/mmorri77/sp77-cse-70477/main/Lec77/src/testMaxHeap4.cpp
+wget https://raw.githubusercontent.com/mmorri21/sp21-cse-70421/main/Lec21/src/testMaxHeap4.cpp
 cp testMaxHeap4.cpp src/testMaxHeap4.cpp
 rm testMaxHeap4.cpp
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec11/inc/MaxHeap.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20212/main/Lec11/inc/MaxHeap.h
 cp MaxHeap.h inc/MaxHeap.h
 rm MaxHeap.h
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec11/inc/MaxHeap2.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20212/main/Lec11/inc/MaxHeap2.h
 cp MaxHeap2.h inc/MaxHeap2.h
 rm MaxHeap2.h
-wget https://raw.githubusercontent.com/mmorri33/sp31-cse-30313/main/Lec11/inc/MaxHeap3.h
+wget https://raw.githubusercontent.com/mmorri33/sp21-cse-30213/main/Lec11/inc/MaxHeap3.h
 cp MaxHeap3.h inc/MaxHeap3.h
 rm MaxHeap3.h
-wget https://raw.githubusercontent.com/mmorri33/sp31-cse-30313/main/Lec11/inc/PriorityQueue.h
+wget https://raw.githubusercontent.com/mmorri33/sp21-cse-30213/main/Lec11/inc/PriorityQueue.h
 cp PriorityQueue.h inc/PriorityQueue.h
 rm PriorityQueue.h
