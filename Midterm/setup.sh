2ee23bf48675cd2794b92b80e05b65cff10acfe5
Matthew Morrison <mmorri22@nd.edu>
2021-03-25 10:56:08 -0400

    Lec 14 Fix

---
+++ Midterm/setup.sh
@@ -0,0 +1,26 @@
+mkdir src inc
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Midterm/Makefile
+make initialize
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Midterm/test.sh
+chmod a+rx test.sh
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Midterm/src/Prob1.cpp
+cp Prob1.cpp src/Prob1.cpp
+rm Prob1.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Midterm/src/Prob2.cpp
+cp Prob2.cpp src/Prob2.cpp
+rm Prob2.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Midterm/src/Prob3.cpp
+cp Prob3.cpp src/Prob3.cpp
+rm Prob3.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Midterm/src/Prob4.cpp
+cp Prob4.cpp src/Prob4.cpp
+rm Prob4.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Midterm/src/Prob5.cpp
+cp Prob5.cpp src/Prob5.cpp
+rm Prob5.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Midterm/inc/SLList.h
+cp SLList.h inc/SLList.h
+rm SLList.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Midterm/inc/SLLNode.h
+cp SLLNode.h inc/SLLNode.h
+rm SLLNode.h
