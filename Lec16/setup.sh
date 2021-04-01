742ad48c2dbe731b90c7a5ef134e391416056582
Matthew Morrison <mmorri22@nd.edu>
2021-04-01 09:54:12 -0400

    Lect16 First Fix

--- Lec16/setup.sh
+++ Lec16/setup.sh
@@ -1,59 +1,71 @@
 mkdir src inc
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec15/Makefile
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec16/Makefile
 make initialize
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec15/test.sh
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec16/test.sh
 chmod a+rx test.sh
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec15/src/Prob3.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec16/src/Prob1.cpp
+cp Prob1.cpp src/Prob1.cpp
+rm Prob1.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec16/src/Prob3.cpp
 cp Prob3.cpp src/Prob3.cpp
 rm Prob3.cpp
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec15/src/DFS.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec16/src/Prob4.cpp
+cp Prob4.cpp src/Prob4.cpp
+rm Prob4.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec16/src/Prob6.cpp
+cp Prob6.cpp src/Prob6.cpp
+rm Prob6.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec16/src/BFS.cpp
+cp BFS.cpp src/BFS.cpp
+rm BFS.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec16/src/BFS2.cpp
+cp BFS2.cpp src/BFS2.cpp
+rm BFS2.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec16/src/DFS.cpp
 cp DFS.cpp src/DFS.cpp
 rm DFS.cpp
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec15/src/DFS2.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec16/src/DFS2.cpp
 cp DFS2.cpp src/DFS2.cpp
 rm DFS2.cpp
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec15/src/Graph.cpp
-cp Graph.cpp src/Graph.cpp
-rm Graph.cpp
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec15/src/testGraph1.cpp
-cp testGraph1.cpp src/testGraph1.cpp
-rm testGraph1.cpp
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec15/src/testVert1.cpp
-cp testVert1.cpp src/testVert1.cpp
-rm testVert1.cpp
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec15/src/testVert2.cpp
-cp testVert2.cpp src/testVert2.cpp
-rm testVert2.cpp
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec15/inc/DynArr.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec16/src/MST.cpp
+cp MST.cpp src/MST.cpp
+rm MST.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec16/src/MST2.cpp
+cp MST2.cpp src/MST2.cpp
+rm MST2.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec16/src/TopSort.cpp
+cp TopSort.cpp src/TopSort.cpp
+rm TopSort.cpp
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec16/inc/DynArr.h
 cp DynArr.h inc/DynArr.h
 rm DynArr.h
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec15/inc/Edge.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec16/inc/Edge.h
 cp Edge.h inc/Edge.h
 rm Edge.h
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec15/inc/Graph1.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec16/inc/Graph1.h
 cp Graph1.h inc/Graph1.h
 rm Graph1.h
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec15/inc/Graph2.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec16/inc/Graph2.h
 cp Graph2.h inc/Graph2.h
 rm Graph2.h
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec15/inc/Graph3.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec16/inc/Graph3.h
 cp Graph3.h inc/Graph3.h
 rm Graph3.h
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec15/inc/Graph4.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec16/inc/Graph4.h
 cp Graph4.h inc/Graph4.h
 rm Graph4.h
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec15/inc/SLList.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec16/inc/SLList.h
 cp SLList.h inc/SLList.h
 rm SLList.h
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec15/inc/stack.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec16/inc/stack.h
 cp stack.h inc/stack.h
 rm stack.h
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec15/inc/Vertex1.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec16/inc/Vertex1.h
 cp Vertex1.h inc/Vertex1.h
 rm Vertex1.h
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec15/inc/Vertex2.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec16/inc/Vertex2.h
 cp Vertex2.h inc/Vertex2.h
 rm Vertex2.h
-wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec15/inc/Vertex.h
+wget https://raw.githubusercontent.com/mmorri22/sp21-cse-20312/main/Lec16/inc/Vertex.h
 cp Vertex.h inc/Vertex.h
 rm Vertex.h
