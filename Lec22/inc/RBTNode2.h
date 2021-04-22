2bbbfb1da451f92caa07188eb3967c62ce62a1b6
Matthew Morrison <mmorri22@nd.edu>
2021-04-22 09:18:23 -0400

    Lecture 22 Update

--- Lec22/inc/RBTNode2.h
+++ Lec22/inc/RBTNode2.h
@@ -73,27 +73,7 @@ struct RBTNode {
 	} 
 	
 	/* Problem 4 Starts Here */
-	// returns pointer to sibling 
-	RBTNode<T>* sibling() const{ 
-	
-		// sibling null if no parent 
-		if (parent == NULL) 
-			return NULL; 
 
-		// Return a pointer to the Sibling
-		return (isOnLeft()) ? parent->right : parent->left; 
-	} 
-	
-	// Get the Node's Uncle 
-	RBTNode<T>* uncle() const{ 
-
-		// If no parent or grandparent, then no uncle 
-		if (parent == NULL or parent->parent == NULL) 
-			return NULL; 
-
-		// Return a pointer to the uncle
-		return (parent->isOnLeft()) ? parent->parent->right : parent->parent->left;
-	}
 
 }; 
 
